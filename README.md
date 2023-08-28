499e91debb5787d781cddde93fd00d23d06fcd75
marshalliu <marshal.liu@gmail.com>
2023-08-28 23:26:53 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# beancount-importers
+<http://blog.heysh.xyz/2019/11/07/netease-youqian-with-beancount/>
+
+## 环境设置
+
+建议使用[Anaconda](https://mirrors.tuna.tsinghua.edu.cn/help/anaconda/)
+来进行环境管理。我之前用的Poetry在Windows下配置出现了一些小问题。
+
+```bash
+conda env create -f environment.yml
+conda activate bc
+```
+当然也可以直接按照`environment.yml`安装需要的Packages。
+
+## Auto initialize some income/expense accounts for Beancount
+
+```bash
+python importers/youqian/accountInit.py > New_Accounts.bean
+# 如果已经建立了一部分账户
+python importers/youqian/accountInit.py Existing_Accounts.bean > New_Accounts.bean
+```
+
+## Import Netease Youqian（网易有钱）
+
+```
+bean-extract importers/youqian/youqian_expense.import documents.tmp/expense.csv > expense.bean
+bean-extract importers/youqian/youqian_income.import documents.tmp/income.csv > income.bean
+```
+
+## Other importers
+
+[smart_importer](https://github.com/beancount/smart_importer) needs to be installed to import smartly.我还添加了jieba分词，对中文也许有作用。
+
+### 微信
+
+数据来源：`支付-钱包-账单-常见问题-下载账单-用作个人对账`
+
+```bash
+bean-extract importers/wechat.import documents.tmp/微信支付账单(xxxxxxxx-xxxxxxxx).csv -e 你的参考账本.bean> test.bean 
+```
+
+### 中国银行借记卡
+
+数据来源：中国银行网上银行（网页版）~~，需要将UTF-16转换成UTF-8~~，现在不需要了。`他们终于找到了按钮！`
+
+```bash
+bean-extract importers/boc.import documents.tmp/test.csv -e 你的参考账本.bean> test.bean
+```
+### 招商银行信用卡
+
+1. `cmb_eml.import`数据源为信用卡账单电子邮件。需要在招行网银上将账单邮寄方式改为“电子邮件（含明细）”，然后在邮件客户端上下载“招商银行信用卡电子账单xxx.eml”
+
+2. `cmb_json.import`数据源为[招商银行信用卡中心](https://xyk.cmbchina.com/credit-express/bill)移动网页，需要保存这个网页与服务器之间`XMLHttpRequest`通信。我写了一篇[blog](https://blog.heysh.xyz/2022/01/21/new-method-to-get-cmb-bill/)来搞到这个。需要将通信内容保存为`.json`文件。
+要是一个不行的话可以试试另外一个。
+
+```bash
+## EML方法
+bean-extract importers/cmb_eml.import documents.tmp/招商银行信用卡电子账单xxx.eml -e 你的参考账本.bean> test.bean
+
+## JSON方法
+bean-extract importers/cmb_json.import documents.tmp/xxx.json -e 你的参考账本.bean > test.bean
+```
+
+### 民生银行借记卡
+
+在手机网上银行上可以导出PDF格式的民生银行借记卡账单。通过`Camelot-py`读取PDF。
+
+>注意，这种方法是按照PDF的绝对位置取数值的，如果你有超大额支出的话请务必核对。我就没有这种烦恼啦~
+
+```bash
+bean-extract importers/cmbc_pdf.import documents.tmp/pdf_mxXXXXXX.pdf -e 你的参考账本.bean> test.bean
+```
+
+### 使用bean-file归档
+
+```bash
+bean-file -o documents importers/XXXX.import documents.tmp/XXXX 
+```
+
+## 可能出现的问题
+
+- 在Windows的CMD下，通过`>`输出的文件编码可能有误，需要在运行命令前设置环境变量：
+
+```CMD
+set PYTHONUTF8=1
+set PYTHONIOENCODING=utf-8:surrogateescape
+```
+
+或者一劳永逸，在控制面版的“区域设置”中勾选“使用UTF-8提供全球语言支持”。See https://www.tutorialexample.com/set-windows-powershell-to-utf-8-encoding-to-fix-gbk-codec-can-not-encode-character-error-powershell-tutorial/
+
+另外感谢[zsxsoft](https://github.com/zsxsoft/my-beancount-scripts)，不得不说，通过eml文件导入账单真是个好主意。~~不再是了，当然还是要感谢。~~ 现在又是了~
---
+++ importers/cmb_eml.py
@@ -0,0 +1,114 @@
+
+"""Importer for 招商银行 (China Merchants Bank)
+"""
+__copyright__ = "Copyright (C) 2019-2021  He Yeshuang"
+__license__ = "GNU GPLv2"
+
+import base64
+import datetime
+import re
+from email import parser, policy
+from os import path
+from datetime import datetime
+
+from beancount.core import data
+from beancount.core.amount import Amount
+from beancount.core.number import D
+from beancount.ingest import importer
+from bs4 import BeautifulSoup
+from dateutil.parser import parse as dateparse
+# from smart_importer import PredictPayees, PredictPostings
+
+
+class CmbEmlImporter(importer.ImporterProtocol):
+    """An importer for CMB .eml files."""
+
+    def __init__(self, account='Liabilities:CreditCards:CMB:Card'):
+        # print(file_type)
+        self.account_name: str = account
+        self.currency = "CNY"
+        pass
+
+    def identify(self, file):
+        # Match if the filename is as downloaded and the header has the unique
+        # fields combination we're looking for.
+        return (
+            re.match(r"招商银行信用卡电子账单", path.basename(file.name)) and
+            re.search(r"eml", path.basename(file.name))
+        )
+
+    def file_name(self, file):
+        return 'cmb.{}'.format(path.basename(file.name))
+
+    def file_account(self, _):
+        return self.account_name
+
+    def extract(self, file, existing_entries=None):
+        # Open the CSV file and create directives.
+        entries = []
+        index = 0
+        with open(file.name, 'rb') as f:
+            eml = parser.BytesParser(policy=policy.default).parse(fp=f)
+            # b = base64.b64decode(eml.get_payload()[0].get_payload())
+            b = eml.get_payload()[0].get_payload(decode=True).decode("UTF8")
+            d = BeautifulSoup(b, "lxml")
+            date_range = d.findAll(text=re.compile(
+                '\d{4}\/\d{1,2}\/\d{1,2}-\d{4}\/\d{1,2}\/\d{1,2}'))[0]
+            transaction_date = dateparse(
+                date_range.split('-')[1].split('(')[0]).date()
+            balance = '-' + d.find(src="https://s3gw.cmbimg.com/lm50_creditbill_prd/bill_templet_resource/"  # 不知道其他人是不是的hash是不是和我一样
+                                   "email_normal_gold_e_bank_20210603_43032c92498745fdbeef630eac905b38")\
+                .parent.parent.find_next_sibling(
+                    'td').select('font')[0].text.replace('￥', '').replace('¥', '').replace(',', '').strip()
+            txn_balance = data.Balance(
+                account=self.account_name,
+                amount=Amount(D(balance), 'CNY'),
+                meta=data.new_metadata(".", 1000),
+                tolerance=None,
+                diff_amount=None,
+                date=transaction_date
+            )
+            entries.append(txn_balance)
+
+            # 现在不知道是什么jbmn,算了算了
+            bands = d.select('#fixBand29 #loopBand2>table>tbody>tr')
+            for band in bands:
+                tds = band.select('td #fixBand15 table table td')
+                if len(tds) == 0:
+                    continue
+                trade_date = tds[1].text.strip()
+                if trade_date == '':
+                    trade_date = tds[2].text.strip()
+                # date = datetime.strptime(trade_date,'%m%d').replace(year=transaction_date.year).date()
+                date = datetime.strptime(trade_date, '%m%d')
+                if date.month == 12 and transaction_date.month == 1:
+                    date = date.replace(year=transaction_date.year-1).date()
+                else:
+                    date = date.replace(year=transaction_date.year).date()
+                full_descriptions = tds[3].text.strip().split('-')
+                payee = full_descriptions[0]
+                narration = '-'.join(full_descriptions[1:])
+                real_currency = 'CNY'
+                real_price = tds[4].text.replace(
+                    '￥', '').replace('\xa0', '').replace('¥', '').strip()
+                # print("Importing {} at {}".format(narration, date))
+                flag = "*"
+                amount = -Amount(D(real_price), real_currency)
+                meta = data.new_metadata(file.name, index)
+                txn = data.Transaction(
+                    meta, date, self.FLAG, payee, narration, data.EMPTY_SET, data.EMPTY_SET, [
+                        data.Posting(self.account_name, amount,
+                                     None, None, None, None),
+                    ])
+
+                entries.append(txn)
+
+        # Insert a final balance check.
+
+        return entries
+
+
+# @PredictPostings()
+# @PredictPayees()
+# class SmartWechatImporter(WechatImporter):
+#     pass

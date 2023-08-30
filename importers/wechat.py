0f37896d5f0c4daed9624afd763a2723fac96327
Marshal Liu <marshal.liu@gmail.com>
2023-08-30 13:40:34 +0800

    优化目录结构，更新wechat导入逻辑增加支付信息

--- importers/wechat.py
+++ importers/wechat.py
@@ -15,32 +15,37 @@ from enum import Enum
 from os import path
 from typing import Dict
 
-from beancount.core import account, data, position
+from beancount.core import data, flags
 from beancount.core.amount import Amount
 from beancount.core.number import ZERO, D
 from beancount.ingest import importer
 from dateutil.parser import parse
 # from smart_importer import PredictPostings, PredictPayees
 
+_COMMENTS_STR = "收款方备注:二维码收款付款方留言:"
+
 class WechatImporter(importer.ImporterProtocol):
     """An importer for Wechat CSV files."""
 
-    def __init__(self, accountDict: Dict):
+    def __init__(self, account="Assets:Flow:EBank:WeChat", accountDict: Dict=None):
         # print(file_type)
+        self.account = account
         self.accountDict = accountDict
+        self.default_set = frozenset({"wechat"})
         self.currency = "CNY"
+        self.source="微信支付"
         pass
 
     def identify(self, file):
         # Match if the filename is as downloaded and the header has the unique
         # fields combination we're looking for.
-        return (re.search(r"微信支付账单", path.basename(file.name)))
+        return re.match(r"微信支付账单\(\d{8}-\d{8}\).csv", path.basename(file.name))
 
     def file_name(self, file):
         return 'wechat.{}'.format(path.basename(file.name))
 
     def file_account(self, _):
-        return "Assets:Flow:EBank:WeChat"
+        return self.account
 
     def file_date(self, file):
         # Extract the statement date from the filename.
@@ -51,16 +56,17 @@ class WechatImporter(importer.ImporterProtocol):
         # Open the CSV file and create directives.
         entries = []
         index = 0
+
         with open(file.name, encoding="utf-8") as f:
             for _ in range(16):
                 next(f)
             for index, row in enumerate(csv.DictReader(f)):
+                dt = parse(row["交易时间"])
                 if "转入零钱通" in row["交易类型"]:
                     continue  # skip the transfer to wechat
 
-                meta = data.new_metadata(file.name, index)
-                date = parse(row['交易时间']).date()
-                #logging.warning("amount=%s", row['金额(元)'])
+                flag = flags.FLAG_WARNING
+                dt = parse(row["交易时间"])
                 raw_amount = D(row['金额(元)'].lstrip("¥"))
                 isExpense = True if (row['收/支'] == '支出' or row['收/支'] == '/') else False
                 if isExpense:
@@ -68,23 +74,56 @@ class WechatImporter(importer.ImporterProtocol):
                 amount = Amount(raw_amount, self.currency)
                 payee = row['交易对方']
                 narration = row['商品']
+                if narration.startswith(_COMMENTS_STR):
+                    narration = narration.replace(_COMMENTS_STR, "")
+                if narration == "/":
+                    narration = ""
+
                 account_1_text = row['支付方式']
                 account_1 = 'Assets.FIXME'
                 for asset_k, asset_v in self.accountDict.items():
-                    if account_1_text.find(asset_k) != -1:
-                        # print(asset_k, asset_v)
+                    if asset_k in account_1_text:
                         account_1 = asset_v
+                        flag = flags.FLAG_OKAY
+
+                # Insert a final balance check.
+                postings = [data.Posting(account_1, amount, None, None, None, None)]
+
+                if row["当前状态"] == "充值完成":
+                    postings.insert(
+                        0,
+                        data.Posting(self.account, -amount, None, None, None, None),
+                    )
+                    narration = "微信零钱充值"
+                    payee = None
+
+                meta = data.new_metadata(
+                    file.name, index, kvlist={
+                        "merchantId": row["商户单号"].rstrip("\t"),
+                        "method":account_1_text.rstrip("\t"),
+                        "orderId": row["交易单号"].rstrip("\t"),
+                        "payTime": row["交易时间"].rstrip("\t"),
+                        "source": self.source.rstrip("\t"),
+                        "status": row["当前状态"].rstrip("\t"),
+                        "txType": row["交易类型"].rstrip("\t"),
+                        "type": row['收/支'].rstrip("\t")
+                        }
+                )
+
+                logging.debug("amount=%s", row['金额(元)'])
 
                 txn = data.Transaction(
-                    meta, date, self.FLAG, payee, narration, data.EMPTY_SET, data.EMPTY_SET, [
-                        data.Posting(account_1, amount,
-                                     None, None, None, None),
-                        #data.Posting(account_2, -amount, None, None, None, None),
-                    ])
+                    meta,
+                    dt.date(),
+                    flag,
+                    payee,
+                    narration,
+                    self.default_set,
+                    data.EMPTY_SET,
+                    postings,
+                )
 
                 entries.append(txn)
-
-        # Insert a final balance check.
 
         return entries
 
